c503fbdd288bef5f2a44dd6ad4f7d95acc549999
David Brown <dcbrown73@yahoo.com>
2019-11-04 13:21:11 -0500

    I've got dictionaries working with classes to do updates via foreach loop

--- dictionaries.cs
+++ dictionaries.cs
@@ -1,13 +1,27 @@
 using System;
 using System.Collections.Generic;
 
+/*
+The following file is to help understand dictionaries and using them to manage lists of objects.
+
+My intention was to use structures instead of classes as I read that working with structures
+is a much faster operations.  The issue I ran into is you cannot update structures within a
+dictionary. (it returns a copy of the struct) Well. You can, by making a copy of the struct
+updating it and then replacing the old struct in the dictionary, but now while completing the
+iterating over the dictionary. It will throw the numeration of the loop off and exploded.  (it 
+will compile, but explode when ran)
+
+If you use class objects within the dictionary, then a reference is returned  as you iterate 
+through the foreach loop allowing you to update the object and not corrupting the enumeration.
+
+ */
 namespace LearningApp {
-    struct MyStruct {
+    class Player {
         public string Name;
         public string CharClass;
         public string CharRace;
         public PlayerStats PlayerStat;
-        public MyStruct(string name, string charclass, string charrace, int s, int a, int i, int w, int h, int c) {
+        public Player(string name, string charclass, string charrace, int s, int a, int i, int w, int h, int c) {
             Name = name;
             CharClass = charclass;
             CharRace = charrace;
@@ -15,19 +29,19 @@ namespace LearningApp {
         }
     }
 
-    struct PlayerStats {
+    class PlayerStats {
         public int Strength;
         public int Agility;
         public int Intellect;
-        public int Widsom;
+        public int Wisdom;
         public int Heath;
         public int Charm;
 
-        public PlayerStats(int s, int a, int i, int w, int h, int c){
+        public PlayerStats(int s, int a, int i, int w, int h, int c) {
             Strength = s;
             Agility = a;
             Intellect = i;
-            Widsom = w;
+            Wisdom = w;
             Heath = h;
             Charm = c;
         }
@@ -35,24 +49,33 @@ namespace LearningApp {
 
     class Program {
         static void Main() {
-            Dictionary<string, MyStruct> PlayerList = new Dictionary<string, MyStruct>();
-            
-            PlayerList.Add("IP1", new MyStruct("Admiral", "Mystic", "Dark-Elf", 80, 120, 60, 40, 90, 50));
-            PlayerList.Add("IP2", new MyStruct("Frag", "Warrior", "Dwarf", 110, 100, 70, 40, 120, 40));
-            PlayerList.Add("IP3", new MyStruct("Jack", "Bard", "Dark-Elf", 70, 120, 65, 40, 90, 80));
-            PlayerList.Add("IP4", new MyStruct("Djinn", "Mage", "Gaunt One", 40, 120, 140, 40, 70, 50));
-            PlayerList.Add("IP5", new MyStruct("Sekhmet", "Priest", "Goblin", 40, 100, 50, 100, 90, 50));
+            Dictionary<string, Player> PlayerList = new Dictionary<string, Player>();
 
-            foreach (KeyValuePair<string, MyStruct> element in PlayerList) {
-                Console.WriteLine(string.Format("{0} is a {1} {2}", element.Value.Name, element.Value.CharRace, element.Value.CharClass));
+            PlayerList.Add("IP1", new Player("Admiral", "Mystic", "Dark-Elf", 80, 120, 60, 40, 90, 50));
+            PlayerList.Add("IP2", new Player("Frag", "Warrior", "Dwarf", 110, 100, 70, 40, 120, 40));
+            PlayerList.Add("IP3", new Player("Jack", "Bard", "Dark-Elf", 70, 120, 65, 40, 90, 80));
+            PlayerList.Add("IP4", new Player("Djinn", "Mage", "Gaunt One", 40, 120, 140, 40, 70, 50));
+            PlayerList.Add("IP5", new Player("Sekhmet", "Priest", "Goblin", 40, 100, 50, 100, 90, 50));
+
+            foreach (KeyValuePair<string, Player> element in PlayerList) {
+                Console.WriteLine(string.Format("{0} is a {1} {2}", element.Value.Name,
+                                                                    element.Value.CharRace,
+                                                                    element.Value.CharClass));
                 Console.WriteLine(string.Format("Strength: {0}, Agility: {1}, Intellect: {2}, Wisdom: {3}, Health: {4}, Charm: {5}",
                                                                                                     element.Value.PlayerStat.Strength,
-                                                                                                    element.Value.PlayerStat.Agility, 
-                                                                                                    element.Value.PlayerStat.Intellect, 
-                                                                                                    element.Value.PlayerStat.Widsom, 
-                                                                                                    element.Value.PlayerStat.Heath, 
+                                                                                                    element.Value.PlayerStat.Agility,
+                                                                                                    element.Value.PlayerStat.Intellect,
+                                                                                                    element.Value.PlayerStat.Wisdom,
+                                                                                                    element.Value.PlayerStat.Heath,
                                                                                                     element.Value.PlayerStat.Charm));
-            }    
+                if (element.Value.Name == "Jack") {
+                    element.Value.CharClass = "Warrior";
+                }
+                //The line below can use element.Value.Name/CharRace/CharClass varables also.
+                Console.WriteLine(string.Format("{0} is a {1} {2}", PlayerList[element.Key].Name,
+                                                                    PlayerList[element.Key].CharRace,
+                                                                    PlayerList[element.Key].CharClass));
+            }
         }
     }
 }
