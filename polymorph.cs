58caba3b71285415c01df05bddcacaeb18250852
ckode <dcbrown73@yahoo.com>
2019-11-22 21:37:15 -0500

    just messing around with random generators and seeds

--- polymorph.cs
+++ polymorph.cs
@@ -16,43 +16,61 @@ using System.Collections.Generic;
   In this c# example, instead of using a base event
   class. I use an Interface that each class implements
   and then create the event list of type Iface (the interface)
+
+  ***NOTE***
+     Ignore the random number calls and how I'm using
+     them in class objects.  It's stupid and a bad
+     idea.  :)
+
   */
 
 namespace PolyMorph {
     // The Interface only discribes the Execute() method.
     public interface Iface {
         public void Execute();
+        public abstract int GetDamage();
     }
+
     // Spell class implementing Iface
     public class Spell : Iface {
-        string spellText = "You cast a spell!";
-        public Spell() { }
+        string spellText = "You cast fireball causing {0} damage!";
+        Random random;
+        public Spell(int seed) { random = new Random(seed); }
 
         public void Execute() {
-            Console.WriteLine(spellText);
+            Console.WriteLine(String.Format(spellText, GetDamage()));
+        }
+        public int GetDamage() {
+            return random.Next(50, 250);
         }
     }
     // Melee class implementing Iface
     public class Melee : Iface {
-        string meleeText = "You swing your axe!";
-        public Melee() { }
+        string meleeText = "You swing your axe causing {0} damage!";
+        Random random;
+        public Melee(int seed) { random = new Random(seed); }
         public void Execute() {
-            Console.WriteLine(meleeText);
+            Console.WriteLine(String.Format(meleeText, GetDamage()));
+        }
+        public int GetDamage() {
+            return random.Next(50, 250);
         }
     }
 
     class Program {
+        public static Random random = new Random();
         static void Main() {
+            
             // Now create a list of of objects of type Iface
             List<Iface> mylist = new List<Iface>();
             // Add objects of both Melee and Spell type.
-            mylist.Add(new Spell());
-            mylist.Add(new Melee());
-            mylist.Add(new Melee());
-            mylist.Add(new Melee());
-            mylist.Add(new Spell());
-            mylist.Add(new Spell());
-            mylist.Add(new Spell());
+            mylist.Add(new Spell(random.Next()));
+            mylist.Add(new Melee(random.Next()));
+            mylist.Add(new Melee(random.Next()));
+            mylist.Add(new Melee(random.Next()));
+            mylist.Add(new Spell(random.Next()));
+            mylist.Add(new Spell(random.Next()));
+            mylist.Add(new Spell(random.Next()));
 
             // Loops through each calling the Iface defined
             // method Execute() on each.
